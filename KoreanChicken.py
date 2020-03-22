210fc059204fe16751757caf46ee84d4cbfe2f38
Akshin Goswami <akshin.goswami@gmail.com>
2020-03-22 12:14:17 +1100

    Create KoreanChicken.py

---
+++ KoreanChicken.py
@@ -0,0 +1,5 @@
+# This software is used to simulate korean style drinking games. The following games are simulated:
+#   -> Flick the bottle cap
+#   -> Guess the number in the bottle cap
+#   -> Titanic
+# The code for the games are in external py files, this file is meant to act as a "main function".
