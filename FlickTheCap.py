37015c8d59ea08917e8a63a7f90d34783073505f
Akshin Goswami <akshin.goswami@gmail.com>
2020-03-22 13:43:40 +1100

    Testing Flick the BottleCap

--- FlickTheCap.py
+++ FlickTheCap.py
@@ -4,26 +4,85 @@
 import random
 import time
 
-# Main function #######################################################################
+# Auxiliary Functions #################################################################
+
+# Generate strength of bottle cap
+def BottleCapStrength():
+    min = 400
+    max = 700
+    random.seed(1)
+    bc_stren = random.randint(min, max)
+
+    return abs(bc_stren)
+
+
+# Generate strength of flick
+def FlickStrength():
+    min = 300
+    max = 400
+    min_flick = 0.1
+    max_flick = 0.2
+
+    random.seed(1)
+    val = random.random()
+    flick_multi = min_flick + (val*(max_flick - min_flick))
+    flick_stren = flick_multi*random.randint(min, max)
+
+    return abs(flick_stren)
+
+# Generate graphic of bottle cap
+def DrawBottleCap(bc_stren, num_flicks, lean_left):
+    ll = lean_left
+
+    height  = 5
+    num_tab = 7
+    char_per_tab = 3
+    num_char = char_per_tab*num_tab
+    width = 2*num_char + num_tab
+    if bc_stren >= 0:
+        # draw the tail bit
+        for i in range(0, num_tab):
+            print(width*" " + i*" "*ll + (num_tab-i)*" "*(not(ll)) + "*")
+
+
+    # draw bottle cap with no tail. ie Game End
+    for i in range(1, height):
+        print("|" + num_tab*"\t" + "|")
+    
+    print("|" + num_char*"_" + "|")
+    print("Number of Flicks = " + str(num_flicks))
+    
+    # switch lean left
+    ll = not(ll)
+
+    return ll
+
+    # Main function #######################################################################
 def FlickTheCap(ListOfNames):
     game_fin = False
     max_players = len(ListOfNames)
     player = 0
     winner = "UNKOWN ERROR"
+    wait_time = 0.75
     # Get the bottle cap strength
     bc_strength = BottleCapStrength()
 
     # number of flicks occured
     num_flicks = 0
 
+    # lean of the bottle cap
+    lean_left = True
+
     # main game loop
     while game_fin == False:
         # get player to play
         print("Your turn, " + ListOfNames[player] + "!")
+        print("Press ENTER to flick!")
+        input()
         
         print("Simulating Flick")
         for i in range (0, 3):
-            time.sleep(0.1)
+            time.sleep(wait_time)
             print("...")
         
         # get flick strength
@@ -31,6 +90,7 @@ def FlickTheCap(ListOfNames):
         bc_strength = bc_strength - flick_strength
         num_flicks = num_flicks + 1
         # draw bottle cap state
+        lean_left = DrawBottleCap(bc_stren=bc_strength, num_flicks=num_flicks, lean_left=lean_left)
 
         # check for winner
         if bc_strength <= 0:
@@ -45,30 +105,3 @@ def FlickTheCap(ListOfNames):
         
 
     return winner
-
-# Auxiliary Functions #################################################################
-
-# Generate strength of bottle cap
-def BottleCapStrength():
-    min = 100
-    max = 300
-    random.seed(1)
-    bc_stren = random.randint(min, max)
-
-    return abs(bc_stren)
-
-
-# Generate strength of flick
-def FlickStrength():
-    min = 100
-    max = 300
-    min_flick = 0.1
-    max_flick = 0.5
-
-    val = random.random()
-    flick_multi = min_flick + (val*(max_flick - min_flick))
-    flick_stren = flick_multi*random.randint(min, max)
-
-    return abs(flick_stren)
-
-# Generate graphic of bottle cap
--- KoreanChicken.py
+++ KoreanChicken.py
@@ -3,3 +3,23 @@
 #   -> Guess the number in the bottle cap
 #   -> Titanic
 # The code for the games are in external py files, this file is meant to act as a "main function".
+
+# Import Files
+import FlickTheCap
+
+# Auxiliary Functions ############################################################################
+def GetListOfNames():
+    print("Enter Player Names Here (note, seperate values with commas ','):")
+    StrOfNames = input()
+    ListOfNames = StrOfNames.split(",")
+    print("Players are: ")
+    print(ListOfNames)
+    return ListOfNames
+
+# Test Functions #################################################################################
+def TestFlickTheCap(ListOfNames):
+    FlickTheCap.FlickTheCap(ListOfNames)
+
+# Main Function ###################################################################################
+LoN = GetListOfNames()
+TestFlickTheCap(LoN)
