0c09f18ef62d99a619a432138dbac726c90c24da
Akshin Goswami <akshin.goswami@gmail.com>
2020-03-22 16:44:46 +1100

    Higher or Lower fully implemented

--- HigherLower.py
+++ HigherLower.py
@@ -1,17 +1,20 @@
 def HigherLower(ListOfNames, winner_id):
+    print("########## HIGHER/LOWER ##########")
+
     print(ListOfNames[winner_id] + " You must select a random number between 0 and 99 inclusive")
 
     # main game loop
     quit  = False
     player_id = 0
-    while not(quit):
+    while quit == False:
         if player_id == winner_id:
+            player_id += 1
             continue # skip if it's on winner
         
         print("Player: " + ListOfNames[player_id] + ", guess the number")
         print("Were you right? (y/n)")
 
-        ch = input
+        ch = input()
 
         if ch is "y":
             loser_id = player_id
@@ -23,4 +26,4 @@ def HigherLower(ListOfNames, winner_id):
         if player_id > (len(ListOfNames) - 1):
             player_id = 0
     
-    return loser_id
+    return [loser_id]
