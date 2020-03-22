4a28e80703435b2d12c7177f432f696b522d72a4
Akshin Goswami <akshin.goswami@gmail.com>
2020-03-22 16:25:43 +1100

    Implemnted Higher or Lower

---
+++ HigherLower.py
@@ -0,0 +1,26 @@
+def HigherLower(ListOfNames, winner_id):
+    print(ListOfNames[winner_id] + " You must select a random number between 0 and 99 inclusive")
+
+    # main game loop
+    quit  = False
+    player_id = 0
+    while not(quit):
+        if player_id == winner_id:
+            continue # skip if it's on winner
+        
+        print("Player: " + ListOfNames[player_id] + ", guess the number")
+        print("Were you right? (y/n)")
+
+        ch = input
+
+        if ch is "y":
+            loser_id = player_id
+            # print lose statement
+            print("DRINK!")
+            quit = True
+
+        player_id += 1
+        if player_id > (len(ListOfNames) - 1):
+            player_id = 0
+    
+    return loser_id
