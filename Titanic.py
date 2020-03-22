04b6ace2bf82fdfb5bac28a24075fbfc323a1305
Sisil Gunasekera <harrygunasekera98@gmail.com>
2020-03-22 19:03:50 +1100

    fixed small error in the tipping code and added basic loop for play

--- Titanic.py
+++ Titanic.py
@@ -3,13 +3,68 @@
 # Import Files
 import math
 from scipy.stats import expon
+import time
 from random import *
+import os
 
 # Auxiliary Functions #################################################################
+def DramaticPause(nDrops):
+	os.system('clear')
+	print("Pouring")
+	time.sleep(1)
+	for i in range(0, nDrops):
+		time.sleep(0.3)
+		print("...")
+	time.sleep(1)
+
+def MakeCup():
+	cupSlice = [' ',' ',' ','||',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','||']
+	cupBase = [' ',' ',' ','||','_','_','_','_','_','_','_','_','_','_','||']
+	cup = []
+	for i in range(0,16):
+		cup.append(cupSlice.copy())
+	cup.append(cupBase)
+	return cup
+
+def PrintCup(cup):
+	os.system('clear')
+	print('\n\n\n')
+	for cSlice in cup:
+		print(' '.join(cSlice))
+
+def FillCupOnce(cup):
+	sliceNum = 15
+	i = 0
+	filled = False
+	while not filled:
+		if cup[sliceNum-i].count('#') < 10:
+			while not filled:
+				randIndex = randint(4,13)
+				if cup[sliceNum-i][randIndex] != '#':
+					cup[sliceNum-i][randIndex] = '#'
+					filled = True
+		else:
+			i = i + 1
+	return cup
+
+def FillNPrintCup(cup, amount, rate):
+	PrintCup(cup)
+	time.sleep(1)
+	for i in range(0, amount):
+		cup = FillCupOnce(cup)
+		PrintCup(cup)
+		time.sleep(rate)
+	time.sleep(1)
+	return cup
+
 
 # Main ################################################################################
 def Titanic(ListOfNames):
 	data_expon = expon.rvs(scale=1,loc=0,size=10000)
+
+	cup = MakeCup()
+
+	cup = FillNPrintCup(cup, 75, 0.02)
 
 	data = []
 	i = 0
@@ -22,24 +77,28 @@ def Titanic(ListOfNames):
 	# anything <= min has 0 chance of tipping
 	# decreasing min increases the effective range of input for tipping
 	currInput = 0
-	total = 120
-	min = 125
+	total = 75
+	min = 100
 	max = 150
 	tipped = False
 
 	while not tipped:
-		inputChoice = int(input("\nPlease select a pouring option...\n1. Small Pour\n2. Medium Pour\n3. Heavy Pour\nChoice (1,2,3): "))
+		inputChoice = int(input("\n Please select a pouring option...\n1. Small Pour\n2. Medium Pour\n3. Heavy Pour\nChoice (1,2,3): "))
+		print()
 		# logic for strength of pour 
 		if inputChoice == 1:
 			currInput = randint(1,2)
+			cup = FillNPrintCup(cup, currInput, 2)
 		elif inputChoice == 2:
 			currInput = randint(2,5)
+			cup = FillNPrintCup(cup, currInput, 1)
 		elif inputChoice == 3:
 			currInput = randint(5,10)
+			cup = FillNPrintCup(cup, currInput, 0.5)
+
 		perc_full = total/max*100
 		total = total + currInput
-		print (currInput)
-		print("Total is ", total)
+
 		if total > min and total < max:
 		  	# The following logic scales the input relative to position between min and max
 		  	dif = max - min
@@ -50,12 +109,11 @@ def Titanic(ListOfNames):
 			Chance = 100
 		else:
 		  	Chance = 0
-		print("For glass " ,perc_full, " full")
-		print(Chance, "Chance of tipping")
+		#print("For glass " ,perc_full, " full")
+		#print(Chance, "Chance of tipping")
 
 		# From chance percentage determine if cup tips
 		x = randint(1, 100)
-		print(x)
 		if x <= Chance:
 		  print("TIP!")
 		  tipped = True
