d06dc67ce2533210b2117185cffe25c5b0320d28
Akshin Goswami <akshin.goswami@gmail.com>
2020-03-22 12:34:35 +1100

    Create FlickTheCap.py

---
+++ FlickTheCap.py
@@ -0,0 +1,12 @@
+# This file is used to simulate the Flick The Cap game as part of Korean Drinking Games
+
+# Main function #######################################################################
+
+
+# Auxiliary Functions #################################################################
+
+# Generate strength of bottle cap
+
+# Generate strength of flick
+
+# Generate graphic of bottle cap
